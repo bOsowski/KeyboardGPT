bb0671b85db0728854c40ae2f13f9277dc15ab6a
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-03-31 20:49:56 -0700

    Added click handlers for the buttons.

--- app/src/main/java/net/bosowski/keyboard/KeyboardService.kt
+++ app/src/main/java/net/bosowski/keyboard/KeyboardService.kt
@@ -1,9 +1,13 @@
 package net.bosowski.keyboard
 
 import android.inputmethodservice.InputMethodService
+import android.view.KeyCharacterMap
+import android.view.KeyEvent
 import android.view.View
 import net.bosowski.R
 import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.view.children
 
 
 class KeyboardService: View.OnClickListener, InputMethodService() {
@@ -12,9 +16,15 @@ class KeyboardService: View.OnClickListener, InputMethodService() {
     @Override
     override fun onCreateInputView(): View {
         val myKeyboardView: View = layoutInflater.inflate(R.layout.keyboard_view, null)
-//        val btnA: Button = myKeyboardView.findViewById(R.id.keyboard_button_g)
-//        btnA.setOnClickListener(this)
-        //ADD ALL THE OTHER LISTENERS HERE FOR ALL THE KEYS
+        val parentLayout = myKeyboardView.findViewById<LinearLayout>(R.id.keyboard)
+        val buttonRows = parentLayout.children
+        for(row in buttonRows){
+            for(button in (row as LinearLayout).children){
+                button.setOnClickListener{
+                    sendDownUpKeyEvents(KeyEvent.keyCodeFromString((button as Button).text.toString()))
+                }
+            }
+        }
         return myKeyboardView
     }
 
