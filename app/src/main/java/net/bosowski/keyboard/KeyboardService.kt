a546c2f3a3bc6e0fa4538f4f15d888b7232e7d8d
Bartosz Osowski <bartosz.osowski@errigal.com>
2023-04-15 20:50:59 +0100

    Now when characters are inputted, a request for autocompletion will be made and the suggestion textView populated with the suggestion.

--- app/src/main/java/net/bosowski/keyboard/KeyboardService.kt
+++ app/src/main/java/net/bosowski/keyboard/KeyboardService.kt
@@ -4,25 +4,39 @@ import android.content.Context
 import android.inputmethodservice.InputMethodService
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.ExtractedTextRequest
 import net.bosowski.R
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.children
-import java.lang.reflect.Field
-
+import io.ktor.client.HttpClient
+import io.ktor.client.request.bearerAuth
+import io.ktor.client.request.post
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import io.ktor.client.request.setBody
 
 class KeyboardService : View.OnClickListener, InputMethodService() {
 
     private var capsOn = false
     private lateinit var mainView: View
+    private lateinit var suggestions: TextView
 
     private var idToken: String? = null
+
 
     @Override
     override fun onCreateInputView(): View {
         mainView = layoutInflater.inflate(R.layout.keyboard_view, null)
-        idToken = getSharedPreferences("net.bosowski.shared", Context.MODE_PRIVATE).getString("idToken", null)
+        idToken = getSharedPreferences("net.bosowski.shared", Context.MODE_PRIVATE).getString(
+            "idToken",
+            null
+        )
+        suggestions = mainView.findViewById(R.id.suggestions)
         return mainView
     }
 
@@ -33,7 +47,7 @@ class KeyboardService : View.OnClickListener, InputMethodService() {
         capsOn = !capsOn
         for (row in mainView.findViewById<LinearLayout>(R.id.keyboard).children) {
             for (button in (row as LinearLayout).children) {
-                if(button is Button && button.text.length == 1){
+                if (button is Button && button.text.length == 1) {
                     if (capsOn) {
                         button.text = button.text.toString().uppercase()
                     } else {
@@ -47,7 +61,7 @@ class KeyboardService : View.OnClickListener, InputMethodService() {
     /**
      * Called by special keys that can have their tag translated to keyCode, eg. "DEL" or "CAPS_LOCK".
      */
-    fun sendTagAsEvent(v: View){
+    fun sendTagAsEvent(v: View) {
         sendDownUpKeyEvents(KeyEvent.keyCodeFromString(v.tag.toString()))
     }
 
@@ -62,13 +76,39 @@ class KeyboardService : View.OnClickListener, InputMethodService() {
     fun onClickButton(v: View) {
         v as TextView
         currentInputConnection.commitText(v.text, 1)
-    }
-
-    fun onClickSuggestion(v: View){
-        v as TextView
-        if(v.text.isNotEmpty()){
-            currentInputConnection.commitText(idToken, 1)
+        GlobalScope.launch {
+            withContext(Dispatchers.Main) {
+                updateSuggestion()
+            }
         }
     }
 
+    private suspend fun updateSuggestion() {
+        val allText = getAllText()
+        val client = HttpClient()
+        val response = client.post("http://192.168.1.217:8080/api/ai/autocompleteRequest") {
+            bearerAuth(idToken ?: "")
+            setBody(allText)
+        }
+        val suggestion = "$allText${response.bodyAsText()}"
+        suggestions.text = suggestion
+    }
+
+    fun onClickSuggestion(v: View) {
+        v as TextView
+        if (v.text.isNotEmpty()) {
+            replaceAllTextWith(v.text.toString())
+        }
+    }
+
+    private fun replaceAllTextWith(replacement: String) {
+//        currentInputConnection.setSelection(0, 0) //todo: check if this is required
+        currentInputConnection.setSelection(0, getAllText().length)
+        currentInputConnection.commitText(replacement, replacement.length)
+    }
+
+    private fun getAllText(): String {
+        val allText = currentInputConnection.getExtractedText(ExtractedTextRequest(), 0)
+        return allText?.text?.toString() ?: ""
+    }
 }
